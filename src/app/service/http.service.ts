6e6f84eee2f0af5356f7a9443c75492b0081d508
Jonas-Flores <jonas-silva8@outlook.com>
2022-02-24 15:07:39 -0300

    add interceptors folder

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { APIResponse, Game } from '../models';
 
 @Injectable({
   providedIn: 'root'
