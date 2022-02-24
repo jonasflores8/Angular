bcd95a8ca6132afb499bf30377fc05822d2f8b1e
Jonas-Flores <jonas-silva8@outlook.com>
2022-02-24 14:07:59 -0300

    adicionado a thumb GTA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
-  {
-    path: 'search/:game-search',
-    component: HomeComponent,
-  }
+  { path: '', component: HomeComponent },
+  { path: 'search/:game-search', component: SearchBarComponent },
 ];
 
 @NgModule({
